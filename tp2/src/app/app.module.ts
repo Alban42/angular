83640e3b4b223201c4ed019c88f13a414a6c27f5
alban <alban.menager@gmail.com>
2022-09-06 17:39:47 +0200

    Use material table with filters

--- tp2/src/app/app.module.ts
+++ tp2/src/app/app.module.ts
@@ -12,6 +12,10 @@ import { PostsComponent } from './posts/posts.component';
 import { PostDetailComponent } from './post-detail/post-detail.component';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatTableModule } from '@angular/material/table';
+import {MatInputModule} from '@angular/material/input';
+import {MatPaginatorModule} from '@angular/material/paginator';
+import {MatSortModule} from '@angular/material/sort';
 
 @NgModule({
   declarations: [
@@ -29,6 +33,10 @@ import { MatFormFieldModule } from '@angular/material/form-field';
     MatListModule,
     MatCardModule,
     MatFormFieldModule,
+    MatTableModule,
+    MatInputModule,
+    MatPaginatorModule,
+    MatSortModule
   ],
   providers: [],
   bootstrap: [AppComponent],
--- tp2/src/app/posts/posts.component.ts
+++ tp2/src/app/posts/posts.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
 import { Router } from '@angular/router';
 import { Post } from '../post';
 import { PostsService } from '../posts.service';
@@ -6,22 +9,32 @@ import { PostsService } from '../posts.service';
 @Component({
   selector: 'app-posts',
   templateUrl: './posts.component.html',
-  styleUrls: ['./posts.component.scss']
+  styleUrls: ['./posts.component.scss'],
 })
-export class PostsComponent implements OnInit {
+export class PostsComponent implements AfterViewInit {
+  displayedColumns: string[] = ['id', 'userId', 'title', 'body'];
+  dataSource: MatTableDataSource<Post>;
 
-  posts?: Post[];
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
 
-  constructor(private postsService: PostsService, private router: Router) { }
+  constructor(private postsService: PostsService, private router: Router) {
+    this.dataSource = new MatTableDataSource();
+  }
 
   ngOnInit(): void {
     this.getPosts();
   }
 
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
+
   getPosts(): void {
-    this.postsService.getPosts().subscribe(
-      posts => this.posts = posts
-    );
+    this.postsService.getPosts().subscribe((posts) => {
+      return (this.dataSource.data = posts);
+    });
   }
 
   selectPost(id: number): void {
@@ -30,12 +43,22 @@ export class PostsComponent implements OnInit {
 
   applyFilter(event: Event) {
     const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
 
-  //   this.posts?.filter(containsValue).
-  // }
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
 
-  // containsValue(element, index, array) {
-  //   return (element >= 10);
- }
+  applyMaxFilter(event: Event) {
+    this.dataSource.filterPredicate = (data: Post, filter: string) =>
+      data.title.length <= Number(filter);
 
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
 }
